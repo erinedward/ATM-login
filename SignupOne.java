96e23cbe3148848591d4c65896c219b935296d88
erinedward <83719179+erinedward@users.noreply.github.com>
2024-08-25 22:36:27 +0530

    Add files via upload

---
+++ SignupOne.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class SignupOne extends JFrame {
+
+    SignupOne(){
+
+        setLayout(null);
+
+        Random ran = new Random();
+        long random = Math.abs((ran.nextLong() % 9000L) + 1000L);
+
+        JLabel formno = new JLabel("APPLICATION FORM NO. " + random);
+        formno.setFont(new Font("Raleway", Font.BOLD, 38));
+        formno.setBounds(140,20,600,40);
+        add(formno);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setSize(850,800);
+        setLocation(350,10);
+        setVisible(true);
+
+    }
+    public static void main(String args[]){
+        new SignupOne();
+    }
+    }
